4b839780c353dbd5a3dbd4a0034bdfc7e3d79464
jess-tech-dev <jessy.kalanga.dev@gmail.com>
2026-06-07 08:58:15 +0100

    Update script.js
    Change script

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
 const menuBtn = document.querySelector(".menu-btn");
-const navUl = document.querySelector("nav ul");
+const navLinks = document.querySelector(".nav-links");
 
 menuBtn.addEventListener("click", () => {
-    navUl.classList.toggle("active");
-    });
-
-const form = document.querySelector("form");
-
-form.addEventListener("submit", (e) => {
-
-    e.preventDefault();
-
-    alert("Thank you for contacting Higherjess Business!");
-
-    form.reset();
-
+    navLinks.classList.toggle("active");
 });
